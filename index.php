aa30d2c11fa55c4eebf80b076a21999e34655ef2
rasyidwi <68262726+rasyidwi@users.noreply.github.com>
2021-03-15 14:51:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,4 @@
 <?php echo "Hello World, Very Peaceful. Example with git-ftp Jenkins" ; 
-echo "Succesfull Build With Jenkins 123435362719 Add Git FTP"; ?>
+echo "Succesfull Build With Jenkins 123435362719 Add Git FTP"; 
+echo "Edit 1... 2.... 3... Okee"; ?>
+
